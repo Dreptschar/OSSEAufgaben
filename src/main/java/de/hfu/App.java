07dc2bc4eb2776133c0da17b59715be53e6a7923
jfk1998 <56309158+jfk1998@users.noreply.github.com>
2020-11-17 16:59:07 +0100

    added javadoc kommentare

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -9,9 +9,15 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
+        /**
+         * Das hier ist ein javadoc kommentar
+         */
         Scanner s = new Scanner(System.in);
         String input = s.nextLine();
         input = input.toUpperCase();
+        /**
+         * Hier wird das ergebnis ausgegeben
+         */
         System.out.println(input);
     }
 
